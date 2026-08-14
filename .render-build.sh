0723036b11c4ecbc2d839350cc7c53b70758520e
ssyygy <ssyygy4@gmail.com>
2026-08-15 00:11:11 +0300

    mmm

--- .render-build.sh
+++ .render-build.sh
@@ -5,4 +5,4 @@ apt-get install -y libgl1-mesa-glx libglib2.0-0
 pip install --upgrade pip
 pip install -r requirements.txt
 
-python -c "from paddleocr import PaddleOCR; PaddleOCR(lang='ru')"
+python -c "from paddleoc import PaddleOCR; PaddleOCR(lang='ru')"
